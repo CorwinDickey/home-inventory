4874158ad6428dfd84f3e531bbfb4e194bbfb69e
CorwinDickey <62188881+CorwinDickey@users.noreply.github.com>
2021-04-04 18:39:24 -0500

    configured axios baseURL and headers

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom'
-import './index.css';
 import App from './App';
+import axios from 'axios'
+
+axios.defaults.baseURL = process.env.REACT_APP_SERVER_URL
+axios.defaults.headers.post['Content-Type'] = 'application/json'
 
 ReactDOM.render(
-  <Router>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </Router>,
-  document.getElementById('root')
+    <Router>
+        <React.StrictMode>
+            <App />
+        </React.StrictMode>
+    </Router>,
+    document.getElementById('root')
 );
