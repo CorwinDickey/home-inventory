dcccff4ce286f9c21100c9e3410fe34f57ffbb85
CorwinDickey <62188881+CorwinDickey@users.noreply.github.com>
2021-04-06 19:22:03 -0500

    created ForgotPassword component

---
+++ front-end/src/components/account/ForgotPassword.jsx
@@ -0,0 +1,59 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import { Formik, Field, Form, ErrorMessage } from 'formik'
+import * as Yup from 'yup'
+
+import accountService from '../../services/account'
+import alertService from '../../services/alert'
+
+function ForgotPassword() {
+    const initialValues = {
+        email: ''
+    }
+
+    const validationSchema = Yup.object().shape({
+        email: Yup.string()
+            .email('Email is invalid')
+            .required('Email is required')
+    })
+
+    const { register, handleSubmit, control, reset } = useForm({
+        resolver: yupResolver(validationSchema)
+    })
+
+    function onSubmit({ email }, { setSubmitting }) {
+        alertService.clear()
+        accountService.forgotPassword(email)
+            .then(() => alertService.success('Please check your email for password reset instructions'))
+            .catch(error => alertService.error(error))
+            .finally(() => setSubmitting(false))
+    }
+
+    return (
+        <Formik initialValues={initialValues} validationSchema={validationSchema} onSubmit={onSubmit}>
+            {({ errors, touched, isSubmitting }) => (
+                <Form>
+                    <h3 className='card-header'>Forgot Password</h3>
+                    <div className='card-body'>
+                        <div className='form-group'>
+                            <label>Email</label>
+                            <Field name='email' type='text' className={'form-control' + (errors.email && touched.email ? ' is-invalid' : '')} />
+                            <ErrorMessage name='email' component='div' className='invalid-feedback' />
+                        </div>
+                        <div className='form-row'>
+                            <div className='form-group col'>
+                                <button type='submit' disabled={isSubmitting} className='btn btn-primary'>
+                                    {isSubmitting && <span className='spinner-border spinner-border-sm mr-1'></span>}
+                                    Submit
+                                </button>
+                                <Link to='login'>Cancel</Link>
+                            </div>
+                        </div>
+                    </div>
+                </Form>
+            )}
+        </Formik>
+    )
+}
+
+export default ForgotPassword
