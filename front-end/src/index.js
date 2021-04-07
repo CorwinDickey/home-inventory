57612048d958e2e00c0271c0deb95d4c46293697
CorwinDickey <62188881+CorwinDickey@users.noreply.github.com>
2021-04-07 09:09:22 -0500

    added history and silent token refresh to index.js

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -4,31 +4,22 @@ import { BrowserRouter as Router } from 'react-router-dom'
 import App from './App';
 import axios from 'axios'
 
+import history from './utils/history';
 import accountService from './services/account'
 
 axios.defaults.baseURL = process.env.REACT_APP_SERVER_URL
 axios.defaults.headers.post['Content-Type'] = 'application/json'
 
-function axiosAuth(url) {
-    const user = accountService.userValue
-    const isLoggedIn = user && user.jwtToken
-    const isApiUrl = url.startsWith(process.env.REACT_APP_SERVER_URL)
-    if (isLoggedIn && isApiUrl) {
-        return (
-            axios.defaults.headers.common['Authorization'] = `Bearer ${user.jwtToken}`
-        )
-    } else {
-        return null
-    }
+accountService.refreshToken().finally(startApp)
+
+function startApp() {
+    render(
+        <Router history={history}>
+            <React.StrictMode>
+                <App />
+            </React.StrictMode>
+        </Router>,
+        document.getElementById('app')
+    )
 }
 
-axiosAuth()
-
-ReactDOM.render(
-    <Router>
-        <React.StrictMode>
-            <App />
-        </React.StrictMode>
-    </Router>,
-    document.getElementById('root')
-);
