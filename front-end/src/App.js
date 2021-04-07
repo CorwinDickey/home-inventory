f1d1a845077a550261d7920eb7a0031abbc5a9f8
CorwinDickey <62188881+CorwinDickey@users.noreply.github.com>
2021-04-07 08:46:10 -0500

    added high level auth/unauth split

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,10 +1,27 @@
-import React from 'react'
-import Routes from './components/Routes'
+import React, { useState, useEffect } from 'react'
 
+import Alert from './components/Alert'
+import AuthApp from './components/AuthApp'
+import UnauthApp from './components/UnauthApp'
+
+import accountService from './services/account'
 
 function App() {
+    const [user, setUser] = useState({})
+
+    useEffect(() => {
+        const subscription = accountService.user.subscribe(x => setUser(x))
+        return subscription.unsubscribe()
+    }, [])
+
     return (
-        <Routes />
+        <div>
+            <Alert />
+            { user && user.jwtToken
+                ? <AuthApp />
+                : <UnauthApp />
+            }
+        </div>
     )
 }
 
