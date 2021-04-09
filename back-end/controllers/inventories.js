5cd685d8139a66db2698c1488a346391068e5d42
CorwinDickey <62188881+CorwinDickey@users.noreply.github.com>
2021-04-08 23:37:29 -0500

    created inventories controller

--- back-end/controllers/inventories.js
+++ back-end/controllers/inventories.js
@@ -2,64 +2,62 @@
 // DEPENDENCIES
 // ==============================================================
 const express = require('express')
-const items = express.Router()
-const Item = require('../models/item')
+const inventories = express.Router()
+const Inventory = require('../models/inventory')
 
 // ==============================================================
 // CREATE ROUTE
 // ==============================================================
-items.post('/', (req, res) => {
-    Item.create(req.body, (error, createdItem) => {
+inventories.post('/', (req, res) => {
+    Inventory.create(req.body, (error, createdInventory) => {
         if (error) {
             res.status(400).json({ error: error.message })
         }
-        res.status(200).send(createdItem)
+        res.status(200).send(createdInventory)
     })
 })
 
 // ==============================================================
 // READ ROUTES
 // ==============================================================
-items.get('/:id', (req, res) => {
-    Item.findOne({'_id': req.params.id}, (error, foundItem) => {
+inventories.get('/:id', (req, res) => {
+    Inventory.findOne({'_id': req.params.id}, (error, foundInventory) => {
         if (error) {
             res.status(400).json({ error: error.message })
         }
-        res.status(200).json(foundItem)
+        res.status(200).json(foundInventory)
     })
 })
 
-items.get('/', (req, res) => {
-    Item.find({}, (error, foundItems) => {
+inventories.get('/', (req, res) => {
+    Inventory.find({}, (error, foundInventories) => {
         if (error) {
             res.status(400).json({ error: error.message })
         }
-        res.status(200).json(foundItems)
+        res.status(200).json(foundInventories)
     })
 })
 
 // ==============================================================
 // DELETE ROUTE
 // ==============================================================
-items.delete('/:id', (req, res) => {
-    Item.findByIdAndDelete(req.params.id, (error, deletedItem) => {
+inventories.delete('/:id', (req, res) => {
+    Inventory.findByIdAndDelete(req.params.id, (error, deletedInventory) => {
         if (error) {
             res.status(400).json({ error: error.message })
         }
-        res.status(200).json(deletedItem)
+        res.status(200).json(deletedInventory)
     })
 })
 
 // ==============================================================
 // UPDATE ROUTE
 // ==============================================================
-items.put('/:id', (req, res) => {
-    Item.findByIdAndUpdate(req.params.id, req.body, { new: true }, (error, updatedItem) => {
+inventories.put('/:id', (req, res) => {
+    Inventory.findByIdAndUpdate(req.params.id, req.body, { new: true }, (error, updatedInventory) => {
         if (error) {
             res.status(400).json({ error: error.message })
         }
-        res.status(200).json(updatedItem)
+        res.status(200).json(updatedInventory)
     })
 })
-
-module.exports = items
